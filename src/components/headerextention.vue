5e9f4e9c015900c03926f993a20922954c4bf512
Benjamin Zimmer <BEZI@BGLOP.lan>
2022-03-21 15:23:02 +0100

    Abgabe bei Gunnar Zwischenstand

---
+++ src/components/headerextention.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <router-link to="/">ISUPPORT</router-link>
+  </header>
+</template>
+
+<script>
+import Nav from "./Nav.vue";
+
+export default {
+  name: "Header",
+  props: {
+  },
+  components: { Nav },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;700&display=swap");
+
+header {
+  display: flex;
+  justify-content: space-between;
+  position: sticky;
+  top: ;
+  backdrop-filter: blur(10px);
+  padding: 0 20px;
+}
